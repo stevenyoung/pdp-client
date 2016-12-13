a53425224bb9e870b371425ba6cbff846ab7dabf
Steven Young <fivepockets@gmail.com>
2016-12-13 12:16:29 -0800

    adding map container spec

---
+++ test/components/mapcontainer.spec.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+import { shallow } from 'enzyme';
+
+import MapContainer from '../../app/components/MapContainer.jsx';
+
+describe('<MapContainer />', () => {
+  it('should contain one map node');
+  it('should display a map given a location');
+});
