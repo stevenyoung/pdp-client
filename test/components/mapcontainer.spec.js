9bdbe2dfd2ef37a4a4c651267151acc941b79152
Steven Young <fivepockets@gmail.com>
2016-12-20 10:52:09 -0800

    test: map container now contains a node for a list of places

--- test/components/mapcontainer.spec.js
+++ test/components/mapcontainer.spec.js
@@ -7,12 +7,13 @@ import { shallow, render } from 'enzyme';
 import MapContainer from '../../app/components/MapContainer.jsx';
 
 describe('<MapContainer />', () => {
-  it('<shallow> should contain one map node', () => {
-    const wrapper = shallow(<MapContainer />);
+  it('<render> should contain one map node', () => {
+    const wrapper = render(<MapContainer />);
     expect(wrapper.find('.leafletmapcontainer')).to.have.length(1);
   });
-  it('<render> should be initialized as leaflet container', () => {
-    const wrapper = render(<MapContainer />);
-    expect(wrapper.find('#leafletmap')).to.have.length(1);
+  it('<shallow> should contain node with a list of locations', () => {
+    const wrapper = shallow(<MapContainer />);
+    expect(wrapper.find('.w-dyn-list')).to.have.length(1);
   });
+  it('should pass props to its children');
 });
