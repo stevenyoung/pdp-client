629d935ecfe7fd0f30bf5b3aa28ba1bf381cc5ee
Steven Young <fivepockets@gmail.com>
2016-12-22 17:03:40 -0800

    render sample location data in the locations list

--- app/components/ResultsSummary.jsx
+++ app/components/ResultsSummary.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 
-const ResultsSummary = () =>
+const ResultsSummary = (props) =>
   (
     <div className="placelistitem w-dyn-list">
       <div className="locationlist w-dyn-items" id="locations-list">
         <div className="location-list-items w-dyn-item">
-          <div className="locationitem">
-            <a className="location-name" href="/location" />
-            <div className="location-work-title" />
-            <div className="location-artist" />
-          </div>
+          {props.places.map((place) =>
+            <div className="locationitem">
+              <a className="location-name" href="/location" >
+                {place.name}
+              </a>
+              <div className="location-work-title">
+                {place.title}
+              </div>
+              <div className="location-artist">
+                {place.artist}
+              </div>
+            </div>
+            )
+          }
         </div>
       </div>
       <div className="w-dyn-empty">
@@ -18,5 +27,9 @@ const ResultsSummary = () =>
       </div>
     </div>
   );
+
+ResultsSummary.propTypes = {
+  places: React.PropTypes.Array
+};
 
 export default ResultsSummary;
