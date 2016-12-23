5aca17be22b3cdd16df6161c7a6f71f7492a51f8
Steven Young <fivepockets@gmail.com>
2016-12-23 10:20:25 -0800

    show an empty list message or the list of places if available

--- app/components/ResultsSummary.jsx
+++ app/components/ResultsSummary.jsx
@@ -1,32 +1,37 @@
 import React from 'react';
 
-const ResultsSummary = (props) =>
-  (
-    <div className="placelistitem w-dyn-list">
+const ResultsSummary = (props) => {
+  let listNodeContent;
+  if (props.places.length > 0) {
+    listNodeContent = (
       <div className="locationlist w-dyn-items" id="locations-list">
         <div className="location-list-items w-dyn-item">
           {props.places.map((place) =>
             <div className="locationitem">
               <a className="location-name" href="/location" >
-                {place.name}
+                {place.scenelocation}
               </a>
               <div className="location-work-title">
                 {place.title}
               </div>
               <div className="location-artist">
-                {place.artist}
+                {place.author}
               </div>
             </div>
             )
           }
         </div>
-      </div>
+      </div>);
+  } else {
+    listNodeContent = (
       <div className="w-dyn-empty">
-        <div className="emplylocationlist">No items found.</div>
+        <div className="emptylocationlist">No items found.</div>
         <a className="more-locations w-button" href="/nearby">more nearby locations...</a>
       </div>
-    </div>
-  );
+    );
+  }
+  return <div className="placelistitem w-dyn-list">{listNodeContent}</div>;
+};
 
 ResultsSummary.propTypes = {
   places: React.PropTypes.Array
