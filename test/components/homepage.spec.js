03d789cedb5c73844405d29de14c403eb87e8db8
Steven Young <fivepockets@gmail.com>
2016-12-26 12:20:12 -0800

    shallow enzyme unit tests for homepage

--- test/components/homepage.spec.js
+++ test/components/homepage.spec.js
@@ -6,16 +6,23 @@ import { shallow } from 'enzyme';
 
 import HomePage from '../../app/components/HomePage.jsx';
 
-describe('<HomePage />', () => {
-  it('[SHALLOW] should create one .home component', () => {
+import ContentSearchInput from '../../app/components/ContentSearchInput.jsx';
+import MapContainer from '../../app/components/MapContainer.jsx';
+import Footer from '../../app/components/Footer.jsx';
+
+describe('<HomePage /> [shallow]', () => {
+  it('should create one .home component', () => {
     const wrapper = shallow(<HomePage />);
     expect(wrapper.is('.home')).to.equal(true);
   });
-
-  it('should have four child nodes', () => {
+  it('should have three child nodes', () => {
     const wrapper = shallow(<HomePage />);
     expect(wrapper.props().children).to.have.length(3);
   });
-  it('should display main home page content');
-  it('should display footer');
+  it('should display search input, map container, and footer', () => {
+    const wrapper = shallow(<HomePage />);
+    expect(wrapper.find(ContentSearchInput)).to.have.length(1);
+    expect(wrapper.find(MapContainer)).to.have.length(1);
+    expect(wrapper.find(Footer)).to.have.length(1);
+  });
 });
