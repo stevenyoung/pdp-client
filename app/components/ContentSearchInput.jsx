658c04214ceabbd888457086fdc618b7bb0384e7
Steven Young <fivepockets@gmail.com>
2016-12-30 10:20:03 -0800

    separate components for the responsive forms

--- app/components/ContentSearchInput.jsx
+++ app/components/ContentSearchInput.jsx
@@ -1,71 +1,63 @@
 import React from 'react';
 
-const ContentSearchInput = () =>
+const MobileSearchForm = (props) =>
   (
-    <div
-      className="searchinput w-form"
+    <form
+      className="mobilesearchform"
     >
+      <input
+        className="mobilesearch w-hidden-main w-hidden-medium w-hidden-small w-input"
+        id="mobile-search-input"
+        maxLength="256"
+        placeholder={props.placeholder}
+        type="text"
+      />
+      <a
+        className="searchformsubmit w-button w-hidden-main w-hidden-medium w-hidden-small"
+      >Go</a>
+    </form>
+  );
+
+const SearchForm = (props) =>
+  (
+    <div className="logosearchformwrapper w-form">
       <form
-        className="mobilesearchform"
-        data-name="Mobile Search From"
-        id="wf-form-Mobile-Search-From"
-        name="wf-form-Mobile-Search-From"
+        className="searchform"
       >
         <input
-          className="mobilesearch w-hidden-main w-hidden-medium w-hidden-small w-input"
-          id="mobile-search-input"
+          autoFocus="autofocus"
+          className="searchforminput w-input"
           maxLength="256"
-          placeholder="Place? Movie? Book? Song?   ..."
+          placeholder={props.placeholder}
+          required="required"
           type="text"
         />
         <a
-          className="searchformsubmit w-button w-hidden-main w-hidden-medium w-hidden-small"
-          data-ix="new-interaction"
+          className="searchformsubmit w-button"
         >Go</a>
       </form>
-      <div className="w-form-done">
-        <div>Thank you! Your submission has been received!</div>
-      </div>
-      <div className="w-form-fail">
-        <div>Oops! Something went wrong while submitting the form</div>
-      </div>
-      <div className="logosearchcontainer w-container">
-        <div className="app-title">
-          <span className="app-title">places.press</span>
-        </div>
-        <div className="logosearchformwrapper w-form">
-          <form
-            className="searchform"
-            data-name="Search Form"
-            id="wf-form-Search-Form"
-            name="wf-form-Search-Form"
-          >
-            <input
-              autoFocus="autofocus"
-              className="searchforminput w-input"
-              data-name="SearchFormInput"
-              id="SearchFormInput"
-              maxLength="256"
-              name="SearchFormInput"
-              placeholder="Place? Movie? Book? Song?"
-              required="required"
-              type="text"
-            />
-            <a
-              className="searchformsubmit w-button"
-              data-ix="new-interaction"
-            >Go</a>
-          </form>
-          <div className="w-form-done">
-            <div>Thank you! Your submission has been received!</div>
-          </div>
-          <div className="w-form-fail">
-            <div>Oops! Something went wrong while submitting the form</div>
-          </div>
-        </div>
-      </div>
     </div>
   );
 
 
+const ContentSearchInput = (props) => {
+  const appTitle = 'places.press';
+  return (
+    <div className="searchinput w-form" >
+      <MobileSearchForm placeholder={props.placeholder} />
+      <div className="logosearchcontainer w-container">
+        <div className="app-title">
+          <span className="app-title">{appTitle}</span>
+        </div>
+        <SearchForm placeholder={props.placeholder} />
+      </div>
+    </div>
+  );
+};
+
+MobileSearchForm.propTypes = { placeholder: React.PropTypes.string };
+SearchForm.propTypes = { placeholder: React.PropTypes.string };
+ContentSearchInput.propTypes = { placeholder: React.PropTypes.string };
+
 export default ContentSearchInput;
+
