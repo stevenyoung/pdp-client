b9c79baf8108b049653f61539ffec1074df509e7
Steven Young <fivepockets@gmail.com>
2017-01-02 18:17:06 -0800

    separate reducer code based on state tree

--- app/reducers/pdp.js
+++ app/reducers/pdp.js
@@ -5,7 +5,7 @@ const initialState = {
   searchResults: []
 };
 
-export default function pdpApp(state = initialState, action) {
+function searchTerm(state = '', action) {
   switch (action.type) {
   case actions.SEARCH_TERM:
     return { ...state, searchTerm: action.text };
@@ -13,3 +13,18 @@ export default function pdpApp(state = initialState, action) {
     return state;
   }
 }
+
+function searchResults(state = [], action) {
+  switch (action.type) {
+  default:
+    return state;
+  }
+}
+
+export default function pdpApp(state = initialState, action) {
+  return {
+    searchTerm: searchTerm(state.searchTerm, action),
+    searchResults: searchResults(state.searchResults, action)
+  };
+}
+
