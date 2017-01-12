24336480add406597b7ab0d32efedaefe5472229
Steven Young <fivepockets@gmail.com>
2017-01-11 19:53:41 -0800

    update basic redux spec to use current store config

--- test/components/pdp-redux.spec.js
+++ test/components/pdp-redux.spec.js
@@ -1,15 +1,17 @@
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
 
-import store from '../../app/store/pdp';
+import configureStore from '../../app/store/configureStore';
 import * as actions from '../../app/actions/pdp';
 
 describe('Pdp Redux Spec', () => {
   it('should get initial state for store', () => {
+    const store = configureStore();
     expect(store.getState().searchResults.length).to.equal(0);
     expect(store.getState().searchTerm).to.equal('');
   });
   it('should set new search text', () => {
+    const store = configureStore();
     expect(store.getState().searchTerm).to.equal('');
     store.dispatch(actions.setSearchTerm('some search term'));
     expect(store.getState().searchTerm).to.equal('some search term');
