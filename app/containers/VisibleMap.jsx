c0b2198bd4b9f8ad37dbf58470148d1b93c01395
Steven Young <fivepockets@gmail.com>
2017-01-11 19:11:50 -0800

    place collection prop updated with fetched places

--- app/containers/VisibleMap.jsx
+++ app/containers/VisibleMap.jsx
@@ -1,16 +1,16 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 
-import { setSearchTerm, fetchPlaces, invalidateSubreddit } from '../actions/pdp';
+import { setSearchTerm, requestPlaces, receivePlaces, fetchPlaces } from '../actions/pdp';
 import ContentSearchInput from '../components/ContentSearchInput';
 import MapContainer from '../components/MapContainer';
 
 class VisibleMap extends Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleRefreshClick = this.handleRefreshClick.bind(this);
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.handleChange = this.handleChange.bind(this);
+  //   this.handleRefreshClick = this.handleRefreshClick.bind(this);
+  // }
 
   componentDidMount() {
     const { dispatch, searchTerm } = this.props;
@@ -24,18 +24,16 @@ class VisibleMap extends Component {
     }
   }
 
-  handleChange(nextSubreddit) {
-    this.props.dispatch(setSearchTerm(nextSubreddit));
-    this.props.dispatch(fetchPlaces(nextSubreddit));
-  }
+  // handleChange(nextSubreddit) {
+  //   this.props.dispatch(setSearchTerm(nextSubreddit));
+  //   this.props.dispatch(fetchPlaces(nextSubreddit));
+  // }
 
-  handleRefreshClick(e) {
-    e.preventDefault();
-
-    const { dispatch, searchTerm } = this.props;
-    dispatch(invalidateSubreddit(selectedSubreddit));
-    dispatch(fetchPlaces(searchTerm));
-  }
+  // handleRefreshClick(e) {
+  //   e.preventDefault();
+  //   const { dispatch, searchTerm } = this.props;
+  //   dispatch(fetchPlaces(searchTerm));
+  // }
 
   render() {
     const mapboxToken = 'pk.eyJ1Ijoic3RldmVueW91bmciLCJhIjoiY2l3anExbW4zMDAyOTJ0cXhwYnlpNGdmZSJ9.sjA5t0UMpCwyfVzZmzBVow';
@@ -45,7 +43,10 @@ class VisibleMap extends Component {
           placeholder="Place? Movie? Book? Song?"
           onUserUpdate={this.updatePlaceCollection}
         />
-        <MapContainer accessToken={mapboxToken}/>
+        <MapContainer
+          accessToken={mapboxToken}
+          placeCollection={this.props.placeCollection}
+        />
       </div>
     );
   }
@@ -53,7 +54,7 @@ class VisibleMap extends Component {
 
 VisibleMap.propTypes = {
   searchTerm: PropTypes.string.isRequired,
-  places: PropTypes.array.isRequired,
+  placeCollection: PropTypes.array.isRequired,
   isFetching: PropTypes.bool.isRequired,
   lastUpdated: PropTypes.number,
   dispatch: PropTypes.func.isRequired
@@ -61,21 +62,27 @@ VisibleMap.propTypes = {
 
 function mapStateToProps(state) {
   const { searchTerm, places } = state;
-  const {
-    isFetching,
-    lastUpdated,
-    items: posts
-  } = places[searchTerm] || {
-    isFetching: true,
-    items: []
-  };
+  const isFetching = false;
+  const placeCollection = places.items;
+
+  // const {
+  //   isFetching,
+  //   lastUpdated,
+  //   items: posts
+  // } = places[searchTerm] || {
+  //   isFetching: true,
+  //   items: []
+  // };
 
   return {
     searchTerm,
-    posts,
-    isFetching,
-    lastUpdated
+    placeCollection,
+    isFetching
   };
 }
+
+VisibleMap.defaultProps = {
+  placeCollection: []
+};
 
 export default connect(mapStateToProps)(VisibleMap);
