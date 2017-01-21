9b1d99fc9f29519a4e6e6cb4be5331a0dd422523
Steven Young <fivepockets@gmail.com>
2017-01-20 20:24:53 -0800

    updating api server url. let’s figure out a better approach.

--- app/actions/pdp.js
+++ app/actions/pdp.js
@@ -31,7 +31,7 @@ export function receivePlaces(query, json) {
 export function fetchPlaces(query) {
   return function (dispatch) {
     dispatch(requestPlaces(query));
-    return fetch(`http://localhost:5000/search/${query}`)
+    return fetch(`https://pdp-proto.herokuapp.com/search/${query}`)
     .then(response => response.json())
     .then(json => dispatch(receivePlaces(query, json)));
   };
