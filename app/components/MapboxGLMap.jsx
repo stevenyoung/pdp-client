2eda7beca46628e4ee4ac571da0377c95b88e3c6
Steven Young <fivepockets@gmail.com>
2017-01-21 19:07:56 -0800

    mapbox jump to selected location

--- app/components/MapboxGLMap.jsx
+++ app/components/MapboxGLMap.jsx
@@ -5,7 +5,21 @@ const mapElem = 'mbglmap';
 
 class MapboxGLMap extends React.Component {
   static propTypes = {
-    accessToken: React.PropTypes.string
+    accessToken: React.PropTypes.string,
+    mapCenter: React.PropTypes.object,
+    places: React.PropTypes.array
+  }
+
+  constructor(props) {
+    super(props);
+    this.mapOptions = {
+      container: mapElem,
+      style: 'mapbox://styles/mapbox/streets-v9',
+      center: [this.props.mapCenter.place.lng, this.props.mapCenter.place.lat],
+      attributionControl: false,
+      zoom: 11,
+      pitch: 30
+    };
   }
 
   componentDidMount() {
@@ -13,13 +27,48 @@ class MapboxGLMap extends React.Component {
     this.initializeMap();
   }
 
-  initializeMap() {
-    const mapOptions = {
-      container: mapElem,
-      style: 'mapbox://styles/mapbox/streets-v9'
-    };
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.mapCenter !== this.props.mapCenter) {
+      this.flyToCoords(nextProps.mapCenter.place);
+    }
+  }
 
-    this.mapboxgl = new mapboxgl.Map(mapOptions);
+  componentDidUpdate(nextProps) {
+    if (nextProps.places !== this.props.places) {
+      this.displayMarkerCollection(this.props.places);
+    }
+  }
+
+  initializeMap() {
+    this.map = new mapboxgl.Map(this.mapOptions);
+    this.addControls();
+  }
+
+  flyToCoords(coords) {
+    this.map.jumpTo({ center: [coords.lng, coords.lat] });
+  }
+
+  addControls() {
+    this.map.addControl(new mapboxgl.NavigationControl(), 'top-left');
+    this.map.addControl((new mapboxgl.AttributionControl({
+        compact: true
+    })), 'top-left');
+  }
+
+  markerEl() {
+    return (
+      <div className="mbglmarker" />
+    );
+  }
+
+  displayMarkerCollection(places) {
+    places.forEach((place) => {
+      const markerCoords = {
+        lat: place.loc.coordinates[1],
+        lng: place.loc.coordinates[0]
+      };
+      this.displayMarker(markerCoords, place.id);
+    });
   }
 
   render() {
