5909324cda224fa9b548266ead4daf82ae3752f5
Steven Young <fivepockets@gmail.com>
2017-01-23 11:56:54 -0800

    reflects updated app state structure

--- test/components/actions-pdp.spec.js
+++ test/components/actions-pdp.spec.js
@@ -4,16 +4,28 @@ import { expect } from 'chai';
 import configureStore from '../../app/store/configureStore';
 import * as actions from '../../app/actions/pdp';
 
-describe('Pdp Redux Spec', () => {
+describe('Pdp Actions Spec', () => {
+  const initialState = {
+    query: {
+      searchTerm: ''
+    },
+    places: [],
+    mapCenter: {
+      place: {
+        lat: 37.749202,
+        lng: -122.41575
+      }
+    }
+  };
   it('should get initial state for store', () => {
-    const store = configureStore();
-    expect(store.getState().searchResults.length).to.equal(0);
-    expect(store.getState().searchTerm).to.equal('');
+    const store = configureStore(initialState);
+    expect(store.getState().places.length).to.equal(0);
+    expect(store.getState().query.searchTerm).to.equal('');
   });
   it('should set new search text', () => {
-    const store = configureStore();
-    expect(store.getState().searchTerm).to.equal('');
+    const store = configureStore(initialState);
+    expect(store.getState().query.searchTerm).to.equal('');
     store.dispatch(actions.setSearchTerm('some search term'));
-    expect(store.getState().searchTerm).to.equal('some search term');
+    expect(store.getState().query.searchTerm).to.equal('some search term');
   });
 });
