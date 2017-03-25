2e4dedbaaf276149727ef5c50a1525aaf26e9121
Steven Young <fivepockets@gmail.com>
2017-03-25 13:28:38 -0700

    shallow render tests for the home page container passing

--- test/containers/home.spec.js
+++ test/containers/home.spec.js
@@ -4,23 +4,44 @@ import { describe, it } from 'mocha';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
 
-import Home from '../../app/containers/Home.jsx';
+import { Home } from '../../app/containers/Home.jsx';
 
 import ContentSearchInput from '../../app/components/ContentSearchInput.jsx';
 import PlaceInfo from '../../app/components/PlaceInfo';
 
-describe('<HomePage /> [shallow]', () => {
+function setup() {
+  const props = {
+    query: {
+      searchTerm: ''
+    },
+    places: [],
+    mapCenter: {
+      place: {
+        lat: 37.749202,
+        lng: -122.41575
+      }
+    },
+    displayPlace: {}
+  };
+  const enzymeWrapper = shallow(<Home {...props} />);
+  return {
+    props,
+    enzymeWrapper
+  };
+}
+
+describe('<Home /> [shallow]', () => {
   it('should create one .home component', () => {
-    const wrapper = shallow(<Home />);
-    expect(wrapper.is('.home')).to.equal(true);
+    const { enzymeWrapper } = setup();
+    expect(enzymeWrapper.is('.home')).to.equal(true);
   });
   it('should have three child nodes', () => {
-    const wrapper = shallow(<Home />);
-    expect(wrapper.props().children).to.have.length(3);
+    const { enzymeWrapper } = setup();
+    expect(enzymeWrapper.props().children).to.have.length(3);
   });
   it('should display search input and selected place information', () => {
-    const wrapper = shallow(<Home />);
-    expect(wrapper.find(ContentSearchInput)).to.have.length(1);
-    expect(wrapper.find(PlaceInfo)).to.have.length(1);
+    const { enzymeWrapper } = setup();
+    expect(enzymeWrapper.find(ContentSearchInput)).to.have.length(1);
+    expect(enzymeWrapper.find(PlaceInfo)).to.have.length(1);
   });
 });
