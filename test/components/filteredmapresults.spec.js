e9c0ab81ee362b0c4c82c604a278eae5667ab740
Steven Young <fivepockets@gmail.com>
2017-03-25 16:09:16 -0700

    pending tests for filtered map results component

---
+++ test/components/filteredmapresults.spec.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import 'babel-polyfill';
+
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+import { shallow } from 'enzyme';
+
+// import { FilteredMapResults } from '../../app/components/FilteredMapResults';
+
+describe('<FilteredMapResults />', () => {
+  it('[shallow] should render one button component');// , () => {
+    // const wrapper = shallow(<FilteredMapResults />);
+    // expect(wrapper.is('.more-locations')).to.equal(true);
+    // });
+  it('[shallow] should define a prop for children');// , () => {
+    // const wrapper = shallow(<FilteredMapResults />);
+    // expect(wrapper.props().children).to.be.defined;
+    // });
+});
