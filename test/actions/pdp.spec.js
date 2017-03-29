cbe282d7adc10a3d45ec10004c6e80558cd2ffa1
Steven Young <fivepockets@gmail.com>
2017-03-28 19:00:53 -0700

    test for actions: requesting places given a query, appending a search term to a query list, update map center

--- test/actions/pdp.spec.js
+++ test/actions/pdp.spec.js
@@ -15,12 +15,17 @@ describe('Pdp Actions Spec', () => {
         lat: 37.749202,
         lng: -122.41575
       }
+    },
+    displayPlace: {},
+    previousQueries: {
+      terms: []
     }
   };
   it('should get initial state for store', () => {
     const store = configureStore(initialState);
     expect(store.getState().places.length).to.equal(0);
     expect(store.getState().query.searchTerm).to.equal('');
+    expect(store.getState().previousQueries.terms.length).to.equal(0);
   });
   it('should set new search text', () => {
     const store = configureStore(initialState);
@@ -28,4 +33,26 @@ describe('Pdp Actions Spec', () => {
     store.dispatch(actions.setSearchTerm('some search term'));
     expect(store.getState().query.searchTerm).to.equal('some search term');
   });
+  it('should request places given a query', () => {
+    const store = configureStore(initialState);
+    store.dispatch(actions.requestPlaces('blue'));
+    expect(store.getState().places.isFetching).to.equal(true);
+  });
+  it('should append search to query list', () => {
+    const store = configureStore(initialState);
+    const previous = { terms: [] };
+    expect(store.getState().previousQueries.terms.length).to.equal(0);
+    store.dispatch(actions.updateQueryList('previously searched', previous));
+    expect(store.getState().previousQueries.terms.length).to.equal(1);
+    expect(store.getState().previousQueries.terms[0]).to.equal('previously searched');
+  });
+  it('should update map center', () => {
+    const store = configureStore(initialState);
+    expect(store.getState().mapCenter.place.lat).to.equal(37.749202);
+    expect(store.getState().mapCenter.place.lng).to.equal(-122.41575);
+    store.dispatch(actions.updateMapCenter({ lat: 0.0000, lng: 0.0000 }));
+    expect(store.getState().mapCenter.place.lat).to.equal(0.0000);
+    expect(store.getState().mapCenter.place.lng).to.equal(0.0000);
+  });
+
 });
